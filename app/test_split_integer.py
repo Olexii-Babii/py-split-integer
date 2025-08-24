1b5c034381613d9973d3007cf75a3659943bb45a
Olexii Babii <babiy0617@gmail.com>
2025-08-24 14:53:08 +0300

    fix_2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -9,11 +9,11 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    value = 16
-    number_of_parts = 4
+    value = 6
+    number_of_parts = 2
     num = value // number_of_parts
     result = split_integer(value=value, number_of_parts=number_of_parts)
-    assert len(result) == number_of_parts and result == [num, num, num, num]
+    assert len(result) == number_of_parts and set(result) == {num}
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
@@ -27,12 +27,12 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     value = 32
     number_of_parts = 6
     result = split_integer(value=value, number_of_parts=number_of_parts)
-    assert result == sorted(result) and (max(result) - min(result) <= 1)
+    assert result == sorted(result)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     value = 4
     number_of_parts = 6
     result = split_integer(value=value, number_of_parts=number_of_parts)
-    assert sum(result[:(number_of_parts - value) - 1]
-               ) == 0 and sum(result[(number_of_parts - value):]) == value
+    assert value < number_of_parts and sum(result[:(number_of_parts - value) - 1]
+                                           ) == 0 and sum(result[(number_of_parts - value):]) == value
